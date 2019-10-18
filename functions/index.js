8697e55bc58668227f5cbc86ebbf9a9a7ef4e66a
Amarjeet Singh <singhamarjeet0045@gmail.com>
2019-10-18 19:24:52 +0530

    final push

--- functions/index.js
+++ functions/index.js
@@ -3,31 +3,15 @@ const admin = require("firebase-admin");
 const cors = require("cors")({ origin: true });
 
 const axios = require("axios");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-/* exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });*/
-/*
-exports.basicOP = functions.https.onRequest((req,res)=>{
-    if(req.method!=='GET'){
-        return res.status(500).json({
-            message:'not allowed'
-        })
-    }
-    res.status(200).json({
-        message:'it worked'
-    })
-    
 
-})*/
 
   
 exports.fetchRequest = functions
   .region("us-central1")
   .https.onRequest((req, res) => {
     const { orderBy } = req.query;
+  
+  
 
 
     if (req.method !== "GET") {
@@ -36,45 +20,32 @@ exports.fetchRequest = functions
       });
     }
     
-  
-  
-    return axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then(response => {
-        response.data.sort((a, b) => {
-            return orderBy === "desc" || "" ? b.id - a.id : a.id - b.id;
+    if(orderBy!=="asc"& orderBy!=="desc"&orderBy!==undefined){
+        return res.status(200).json({
+            message: "url is not allowded"
           });
-          return res.status(200).json(response.data);
-      })
-      .catch(err => {
-        return res.status(401).json({
-          message:err
-        });
-      });
-    /*
-      if(orderBy==="asc"||orderBy=="desc"){
-        return axios
-        .get("https://jsonplaceholder.typicode.com/todos")
-        .then(response => {
-          response.data.sort((a, b) => {
-              return orderBy === "desc" || "" ? b.id - a.id : a.id - b.id;
-            });
-            return res.status(200).json(response.data);
-        })
-        .catch(err => {
-          return res.status(401).json({
-            message:err
-          });
-        });
-      }
-    
-   
 
+    }
    
-   else{
-    return res.status(405).json({
-        message: "Not allowed"
+    return axios
+    .get("https://jsonplaceholder.typicode.com/todos")
+    .then(response => {
+      response.data.sort((a, b) => {
+          return orderBy === "desc" || "" ? b.id - a.id : a.id - b.id;
+        });
+        return res.status(200).json(response.data);
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message:err
       });
-   }*/
+    });
+
+ 
+    
+      
+      
+    
+  
      
   });
